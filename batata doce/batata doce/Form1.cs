c96dcaba3b2578bc059409b9a8bf97561a9f1254
mwangoclick <adao.gouveia@mwnagoclick.co.ao>
2017-01-11 09:09:52 +0000

    agora é que a batata vai pegar fogo

--- batata doce/batata doce/Form1.cs
+++ batata doce/batata doce/Form1.cs
@@ -16,5 +16,10 @@ namespace batata_doce
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          ///  bolacha
+        }
     }
 }
